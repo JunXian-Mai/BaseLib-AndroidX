985f0e5ac48031d631de71bcdba9882b18c17ae9
maijunxian <markensic@outlook.com>
2020-12-17 11:06:11 +0800

    优化代码逻辑

--- baselibrary/src/main/java/org/markensic/baselibrary/impl/framework/TestMain.kt
+++ baselibrary/src/main/java/org/markensic/baselibrary/impl/framework/TestMain.kt
@@ -1,17 +1,8 @@
 package org.markensic.baselibrary.impl.framework
 
-val biBao: () -> (() -> Int) = {
-  var count = 0
-  { count++ }
-}
-
 fun main() {
-  val fun1 = biBao()
-  val fun2 = biBao()
-  println(fun1())
-  println(fun1())
-  println(fun1())
-  println(fun2())
-  println(fun2())
-  println(fun2())
+  val b = ResizableCapacityLinkedBlockIngQueue<Runnable>(1000)
+  println(b.getCapacity())
+  b.setCapacity(999)
+  println(b.getCapacity())
 }
